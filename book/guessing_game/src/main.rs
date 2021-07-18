06db7a668874872723330b8daeb9a1b7cde87e39
Iman Irfan <kazbrekker@pop-os.localdomain>
2021-07-19 00:14:36 +0800

    guessing game revisit

--- book/guessing_game/src/main.rs
+++ book/guessing_game/src/main.rs
@@ -1,36 +1,15 @@
 use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
 
 fn main() {
-    let secret_number = rand::thread_rng().gen_range(0, 100);
+    println!("Guess the number!");
+    println!("Please input your guess.");
 
-    loop {
-        let mut guess = String::new();
+    // mut mean the variable can be change
+    let mut guess = String::new();
 
-        println!("Guess a number: ");
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        // shadowing
-        let guess: u32 = match guess.trim().parse() {
-            Ok(nun) => nun,
-            Err(_) => continue
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => {
-                println!("Too small!");
-            },
-            Ordering::Greater => {
-                println!("Too big");
-            },
-            Ordering::Equal => {
-                println!("Correct!");
-                break;
-            },
-        }
-
-        println!("You guessed: {}\n\n", guess);
-    }
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    
+    println!("You guessed: {}", guess);
 }
