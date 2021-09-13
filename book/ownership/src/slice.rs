d5953ef9938eeea6045b91b6e54379f6716c3e84
imnirfn <s3ns3xy@yahoo.com>
2021-09-13 11:39:02 +0800

    add find second_word function

--- book/ownership/src/slice.rs
+++ book/ownership/src/slice.rs
@@ -9,15 +9,18 @@ fn main() {
     let s = String::from("hello world");
 
     // slice examples
-    let a = &s[0..5];
-    println!("{}", a);
+    // let a = &s[0..5];
+    // println!("{}", a);
 
-    let string_literal = "holla world";
-    let literal_slice = &string_literal[0..5];
-    println!("{}", literal_slice);
+    // let string_literal = "holla world";
+    // let literal_slice = &string_literal[0..5];
+    // println!("{}", literal_slice);
 
     let word = first_word(&string_literal);
     println!("First word is: {}", word);
+
+    let word_two = second_word(&string_literal);
+    println!("Second word is: {}", word_two);
 
 }
 
@@ -29,10 +32,27 @@ fn first_word(s: &str) -> &str {
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' { // search bytes that represents space
-            println!("in here");
             return &s[0..i]; // returns until found space bytes
         }
     }
 
     &s[..] // returns the entire string
 }
+
+fn second_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    let mut first_index: usize = 0;
+    let mut found_first = false;
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' && !found_first {
+            first_index = i + 1;
+            found_first = true;
+        } else if item == b' ' && found_first {
+            return &s[first_index..i];
+        }
+    }
+
+    ""
+}
+
+
