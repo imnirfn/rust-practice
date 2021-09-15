3d58df271e3b121444f021e6fc4e559691702b4e
imnirfn <s3ns3xy@yahoo.com>
2021-09-15 08:49:44 +0800

    enum chapter 6

--- book/06-enums/src/main.rs
+++ book/06-enums/src/main.rs
@@ -1,11 +1,19 @@
 
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
 }
 
 fn main() {
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+
+    let home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let loopback = IpAddrKind::V6(String::from("::1"));
 
 }
