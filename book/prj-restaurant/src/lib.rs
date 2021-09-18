ba475de8c727ea2ada698b4c716390d12851453e
imnirfn <s3ns3xy@yahoo.com>
2021-09-18 09:16:58 +0800

    restaurant module prj

--- book/prj-restaurant/src/lib.rs
+++ book/prj-restaurant/src/lib.rs
@@ -1,3 +1,4 @@
+
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -14,10 +15,50 @@ mod front_of_house {
     // }
 }
 
+mod back_of_house {
+    #[derive(Debug)]
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn serve_order() {}
+
+    fn fix_incorrect_order() {
+
+    }
+}
+
+use crate::front_of_house::hosting; // shortcut for line 48
+
 pub fn eat_at_restaurant() {
     // absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
 
     // relative path
-    front_of_house::hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+
+    let order_1 = back_of_house::Appetizer::Soup;
+    println!("{:#?}", order_1);
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 }
+
+
+
