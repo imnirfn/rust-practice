16f4843bc9473eae62410bc9b8cf731164b64212
imnirfn <s3ns3xy@yahoo.com>
2021-09-20 08:59:07 +0800

    starting with vectors now wowiess

---
+++ book/08-vectors/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    // explicitly declare the vectors
+    let v: Vec<i32> = Vec::new();
+
+    let v_inferred = vec![1, 2, 3];
+
+    let mut v_mutable = Vec::new();
+
+    v_mutable.push(5);
+    v_mutable.push(6);
+    v_mutable.push(7);
+
+    let v = vec![1, 2, 3, 4, 5];
+    let third: &i32 = &v[2]; // reading vectors content
+    
+    println!("The third element is {}", third);
+
+    match v.get(2) {
+        Some(third) => println!("Third element is: {}", third),
+        None => println!("Theres no third element"),
+    }
+
+    // looping over vectors (not mutable)
+    for i in &v {
+        println!("{}", i);
+    }
+
+    // looping over vectors & changing values (mutable)
+    for i in &mut v_mutable {
+        // * is a deference operator to get value in i
+        // before using += operatot
+        *i += 50;
+        println!("mut {}", i);
+    }
+
+}
