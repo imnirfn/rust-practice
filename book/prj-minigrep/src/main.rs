ebfbb48e3246f80185903750a4abf0140fea4ec0
imnirfn <s3ns3xy@yahoo.com>
2022-03-13 09:55:09 +0800

    printing errors into standard output

--- book/prj-minigrep/src/main.rs
+++ book/prj-minigrep/src/main.rs
@@ -28,12 +28,12 @@ fn main() {
 
     // parsing config variable into main
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {}", err);
+        eprintln!("problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 
