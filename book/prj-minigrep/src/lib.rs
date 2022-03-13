dd4ca10a4725b88e7a12d6056bda13d93ff26529
imnirfn <s3ns3xy@yahoo.com>
2022-03-13 09:49:53 +0800

    implement environment variables

--- book/prj-minigrep/src/lib.rs
+++ book/prj-minigrep/src/lib.rs
@@ -1,9 +1,11 @@
 use std::error::Error;
 use std::fs;
+use std::env;
 
 pub struct Config {
     pub query: String,
-    pub filename: String
+    pub filename: String,
+    pub case_sensitive: bool
 }
 
 impl Config {
@@ -13,8 +15,10 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVES").is_err();
         
-        Ok(Config {query, filename})
+        Ok(Config {query, filename, case_sensitive})
     }
 }
 
@@ -22,7 +26,13 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // reading a file
     let data = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &data) {
+    let results = if config.case_sensitive {
+        search(&config.query, &data)
+    } else {
+        search_case_insensitives(&config.query, &data)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
