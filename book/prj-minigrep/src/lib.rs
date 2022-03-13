febab3a2177c3dca476a98c1284a12ef4786d7a7
imnirfn <s3ns3xy@yahoo.com>
2022-03-13 09:12:43 +0800

    implement a TDD

--- book/prj-minigrep/src/lib.rs
+++ book/prj-minigrep/src/lib.rs
@@ -1,13 +1,13 @@
 use std::error::Error;
 use std::fs;
 
-struct Config {
-    query: String,
-    filename: String
+pub struct Config {
+    pub query: String,
+    pub filename: String
 }
 
 impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
@@ -18,10 +18,46 @@ impl Config {
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     // reading a file
     let data = fs::read_to_string(config.filename)?;
-    println!("contents ->\n{}", data);
+
+    for line in search(&config.query, &data) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
+
+// TODD
+// 1. refactor search funtion using iterators
+//
+
+pub fn search<'a>(query: &str, data: &'a str) -> Vec<&'a str> { //tell rust this will be return from a split data string, thats why we are usig explicit lifetime
+    let mut results = Vec::new();
+    for line in data.lines() {
+        // println!("line: {}, data: {}", query, data);
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let data = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, data));
+    }
+}
+
+
