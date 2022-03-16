87377ffac7df6f27857c051fe117948fef745d06
imnirfn <s3ns3xy@yahoo.com>
2022-03-16 23:15:28 +0800

    exercism exercises

--- exercism/rust/assembly-line/README.md
+++ exercism/rust/assembly-line/README.md
@@ -48,7 +48,10 @@ Converting from a floating point to an integer **will** lose everything behind t
 
 In this exercise you'll be writing code to analyze the production of an assembly line in a car factory. The assembly line's speed can range from `0` (off) to `10` (maximum).
 
-At its lowest speed (`1`), `221` cars are produced each hour. The production increases linearly with the speed. So with the speed set to `4`, it should produce `4 * 221 = 884` cars per hour. However, higher speeds increase the likelihood that faulty cars are produced, which then have to be discarded. The following table shows how speed influences the success rate:
+At its lowest speed (`1`), `221` cars are produced each hour. The production increases linearly with the speed. 
+So with the speed set to `4`, it should produce `4 * 221 = 884` cars per hour. 
+However, higher speeds increase the likelihood that faulty cars are produced, 
+which then have to be discarded. The following table shows how speed influences the success rate:
 
 - `1` to `4`: 100% success rate.
 - `5` to `8`: 90% success rate.
@@ -58,7 +61,7 @@ You have two tasks.
 
 ## 1. Calculate the production rate per hour
 
-Implement a method to calculate the assembly line's production rate per hour, taking into account its success rate:
+Implement [a](a) method to calculate the assembly line's production rate per hour, taking into account its success rate:
 
 ```rust
 assembly_line::production_rate_per_hour(6)
--- exercism/rust/assembly-line/src/lib.rs
+++ exercism/rust/assembly-line/src/lib.rs
@@ -1,11 +1,45 @@
 // This stub file contains items that aren't used yet; feel free to remove this module attribute
 // to enable stricter warnings.
-#![allow(unused)]
+//
+
+const CAR_PRODUCED_PER_HOUR: u8 = 221;
 
 pub fn production_rate_per_hour(speed: u8) -> f64 {
-    unimplemented!("calculate hourly production rate at speed: {}", speed)
+    let prod_rate: f64;
+    prod_rate = speed as f64 * CAR_PRODUCED_PER_HOUR as f64;
+    match speed {
+        1..=4 => {
+            let prod_rate = (prod_rate * 100.0).round() / 100.0;
+            prod_rate
+        },
+        5..=8 => {
+            let prod_rate = (prod_rate * 90.0).round() / 100.0;
+            prod_rate
+        },
+        9..=10 => {
+            let prod_rate = (prod_rate * 77.0).round() / 100.0;
+            prod_rate
+        },
+        _ => 0.0
+    }
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    unimplemented!("calculate the amount of working items at speed: {}", speed)
+    let prod_rate: f64;
+    prod_rate = (speed as f64 * (CAR_PRODUCED_PER_HOUR as f64 /60.0)) as f64;
+    match speed {
+        1..=4 => {
+            let prod_rate = (prod_rate as f64 * 100.0).round() / 100.0;
+            prod_rate as u32
+        },
+        5..=8 => {
+            let prod_rate = (prod_rate as f64 * 90.0).round() / 100.0;
+            prod_rate as u32
+        },
+        9..=10 => {
+            let prod_rate = (prod_rate as f64 * 77.0).round() / 100.0;
+            prod_rate as u32
+        },
+        _ => 0
+    }
 }
